69530b0b46b895f3a8fbb8d165084ce71ec48c67
Daniel <danielirwin99@gmail.com>
2023-03-16 12:56:08 +1100

    Building the Home Page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,17 @@
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <header>
+      <div>
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className="cursor-pointer object-contain"
+        />
+      </div>
+      <div></div>
+    </header>
+  );
+};
 
-export default Login
+export default Login;
