61fd893c5245f3b2e56c99abf9af730b6e22bfde
Daniel <danielirwin99@gmail.com>
2023-03-23 10:18:19 +1100

    aaa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -104,7 +104,7 @@ export default function Home({
       </Head>
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-10">
-        <Banner trendingNow={trendingNow} />
+        <Banner netflixOriginals={netflixOriginals} />
         <section className="md:space-y-16">
           {/* Passing Values for each row to display */}
           <Row title="Trending Now" movies={trendingNow} />
