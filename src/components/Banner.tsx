df06c6db8641a7c87aba1cad0d3801803e05221c
Daniel <danielirwin99@gmail.com>
2023-03-22 18:17:46 +1100

    Deploy

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -22,7 +22,7 @@ const Banner = ({ netflixOriginals }: Props) => {
   useEffect(() => {
     setMovie(
       // this is generating a random movie every time the browser loads
-      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length!)]
     );
     // This is dependant on the netflixOriginals
   }, [netflixOriginals]);
